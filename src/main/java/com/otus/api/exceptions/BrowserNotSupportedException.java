877774d7d957e860ee68e91e254be03d815c1b30
Andrey Vorobev <mammin.xaker@yandex.ru>
2024-02-01 22:48:09 +0300

    Clear

--- src/main/java/com/otus/api/exceptions/BrowserNotSupportedException.java
+++
@@ -1,8 +0,0 @@
-package com.otus.api.exceptions;
-
-public class BrowserNotSupportedException extends RuntimeException {
-
-  public BrowserNotSupportedException(String browserName) {
-    super(String.format("Browser %s not supported", browserName));
-  }
-}
--- src/main/java/com/otus/api/factories/IFactory.java
+++
@@ -1,7 +0,0 @@
-package com.otus.api.factories;
-
-import com.otus.api.exceptions.BrowserNotSupportedException;
-
-public interface IFactory<T> {
-  T create(String browserName) throws BrowserNotSupportedException;
-}
